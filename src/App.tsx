06c22d21bda7b16c031c3f012a16b86e2dc80aed
estroxgr <alexanastagr@gmail.com>
2025-03-28 22:04:14 +0200

    desktop wallpaper

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import useTheme from "@hooks/useTheme";
+
+// components
 import { Taskbar } from "@components/Taskbar";
 
 function App() {
+
+  const {wallpaper} = useTheme();
+
+
   return (
-    <>
+    <div className="desktop" data-bg={wallpaper}>
       <Taskbar />
-    </>
+
+
+    </div>
   );
 }
 

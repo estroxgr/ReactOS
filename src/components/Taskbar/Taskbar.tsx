348a193763c46d81a129c44733d25e8560ec41c8
estroxgr <alexanastagr@gmail.com>
2025-03-28 22:58:39 +0200

    taskbar darkmode style

--- src/components/Taskbar/Taskbar.tsx
+++ src/components/Taskbar/Taskbar.tsx
@@ -1,27 +1,38 @@
 import { CSSProperties } from "react";
 
+// hooks
+import useTheme from "@hooks/useTheme";
+
 // taskbar parts
 import StartBtn from "@components/Taskbar/parts/StartBtn";
 import Datetime from "@components/Taskbar/parts/Datetime";
+import Switcher from "@components/Taskbar/parts/Switcher";
 
 function Taskbar() {
+
+  const { darkMode } = useTheme();
+
   return (
-    <div style={style}>
-      <StartBtn/>
+    <div style={style(darkMode)}>
+      <StartBtn />
       <div></div>
       <Datetime />
+      <Switcher />
     </div>
   );
 }
 
-const style: CSSProperties = {
+const style = (darkMode: boolean): CSSProperties => ({
   position: "absolute",
-  backgroundColor: "#fff",
-  display:'grid',
-  gridTemplateColumns:'140px 1fr 200px',
+  backgroundColor: darkMode ? "#000" : "#fff",
+  color: darkMode ? "#fff" : "#000",
+  display: "grid",
+  gridTemplateColumns: "140px 1fr 150px 27px",
+  justifyContent: "center",
+  alignItems: "center",
   width: "100%",
-  height: 25,
+  height: 40,
   top: 0,
-};
+});
 
 export default Taskbar;

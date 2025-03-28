6d87ccf359ef1567684bea394ed66e7265895802
estroxgr <alexanastagr@gmail.com>
2025-03-28 22:26:00 +0200

    setlocale method hook

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -5,6 +5,7 @@ import { setDarkMode,setBackground } from "@store/reducers/themeSlice";
 
 const useTheme = () => {
     const wallpaper = useSelector((state: RootState) => state.theme.background);
+    const locale = useSelector((state: RootState) => state.theme.locale);
     const darkMode = useSelector((state: RootState) => state.theme.darkMode);
     const dispatch = useDispatch();
 
@@ -16,7 +17,11 @@ const useTheme = () => {
         dispatch(setBackground(newBg));
     }
 
-    return { darkMode, wallpaper,setWallpaper,toggleDarkMode };
+    const setLocale = (locale:string) => {
+        dispatch(setBackground(locale));
+    }
+
+    return { darkMode, wallpaper,setWallpaper,toggleDarkMode,locale,setLocale };
 }
 
 export default useTheme;

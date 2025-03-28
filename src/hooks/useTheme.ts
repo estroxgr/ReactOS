13b3adce39ff18ec97f9e36701e8516c1bc6b3b8
estroxgr <alexanastagr@gmail.com>
2025-03-28 22:03:43 +0200

    theme hook

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,9 +1,10 @@
 import { useSelector,useDispatch } from "react-redux";
 import { RootState } from "@store/store";
-import { setDarkMode } from "@store/reducers/themeSlice";
+import { setDarkMode,setBackground } from "@store/reducers/themeSlice";
 
 
 const useTheme = () => {
+    const wallpaper = useSelector((state: RootState) => state.theme.background);
     const darkMode = useSelector((state: RootState) => state.theme.darkMode);
     const dispatch = useDispatch();
 
@@ -11,7 +12,11 @@ const useTheme = () => {
         dispatch(setDarkMode(!darkMode));
     };
 
-    return { darkMode, toggleDarkMode };
+    const setWallpaper = (newBg:string) => {
+        dispatch(setBackground(newBg));
+    }
+
+    return { darkMode, wallpaper,setWallpaper,toggleDarkMode };
 }
 
 export default useTheme;

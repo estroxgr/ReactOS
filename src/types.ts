6fe1af6e62eb4978c5b6a6b4bf783310c7071a18
estroxgr <alexanastagr@gmail.com>
2025-03-29 09:38:18 +0200

    general types

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+import { CSSProperties } from "react";
+
+/**
+ *   DESKTOP ICONS
+ *
+ */
+
+export interface DIStyle {
+  areabox: CSSProperties;
+  icon: CSSProperties;
+  label: CSSProperties;
+}
+
+export interface DIProps {
+  icon: string;
+}
